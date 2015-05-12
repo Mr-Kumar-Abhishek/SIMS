845fb403e6dc23e95916e322036a9938ec783bd2
Abhishek Kumar <kumar.abhishek@techie.com>
2015-05-12 18:04:17 +0530

    initial commit

---
+++ student-ims/student-ims/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+   
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        Me.Hide()
+        Timer1.Stop()
+        MDI.Show()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Timer1.Start()
+    End Sub
+End Class
