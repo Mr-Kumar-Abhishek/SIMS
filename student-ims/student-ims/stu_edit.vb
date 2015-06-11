3dcd88890a0db2cdc3ebcf7bc39c22ba6bf3f8a4
Abhishek Kumar <kumar.abhishek@techie.com>
2015-06-11 17:38:30 +0530

    removed useless subs

--- student-ims/student-ims/stu_edit.vb
+++ student-ims/student-ims/stu_edit.vb
@@ -1,14 +1,4 @@
 ﻿Public Class stu_edit
 
-    Private Sub stu_edit_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub Label7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label7.Click
-
-    End Sub
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-
-    End Sub
 End Class
