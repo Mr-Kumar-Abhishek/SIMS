f9595a5903914cccb954cd81da8b14a804b38173
Abhishek Kumar <kumar.abhishek@techie.com>
2015-06-25 15:49:29 +0530

    removed useless subs

--- student-ims/student-ims/Certificate Form.vb
+++ student-ims/student-ims/Certificate Form.vb
@@ -1,14 +1,3 @@
 ﻿Public Class Certificate_Form
 
-    Private Sub Label6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label6.Click
-
-    End Sub
-
-    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub Certificate_Form_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
