97e3fdb8b0eda27a77ced6133f43aa12677bf580
Abhishek Kumar <kumar.abhishek@techie.com>
2015-06-25 15:40:32 +0530

    added new renamed files

---
+++ student-ims/student-ims/Payment_Details.vb
@@ -0,0 +1,6 @@
+﻿Public Class Payment_Details
+
+    Private Sub Payment_Details_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Beige
+    End Sub
+End Class
