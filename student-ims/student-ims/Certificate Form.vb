266766b4922809cc6eca4fa7edf3f48834d3852f
Abhishek Kumar <kumar.abhishek@techie.com>
2015-06-30 17:16:20 +0530

    removed extra connection in db

--- student-ims/student-ims/Certificate Form.vb
+++ student-ims/student-ims/Certificate Form.vb
@@ -14,7 +14,6 @@ Public Class Certificate_Form
     Dim ds As DataSet
 
     Private Sub Certificate_Form_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        cn = c.getcon()
         If Not cn.State = ConnectionState.Open Then
             cn.Open()
         End If
