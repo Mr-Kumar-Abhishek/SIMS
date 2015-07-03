11a300180d2e5d5a09e54687e05dde7898f49b30
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-03 17:25:24 +0530

    partial solution of ussue #23 and fixed a database error

--- student-ims/student-ims/modulo.vb
+++ student-ims/student-ims/modulo.vb
@@ -4,6 +4,11 @@ Public Class modulo
     Dim cn As OleDbConnection
     Dim c As New conn
     Dim cm As OleDbCommand
+    Dim dr As OleDbDataReader
+    Dim moduleid(course.nom.Value) As String
+    Dim coursecode(course.nom.Value) As String
+    Dim modulename(course.nom.Value) As String
+    Dim moduledesc(course.nom.Value) As String
 
     Private Sub clearing()
         modid.Text = ""
@@ -21,14 +26,38 @@ Public Class modulo
             nxtbtn.Text = "submit"
             clearing()
         ElseIf pages > course.nom.Value - 1 Then
+            cn = c.getcon()
+            For i As Integer = 0 To course.nom.Value Step 1
+                cm = New OleDbCommand("insert into modulo values ('" + moduleid(i) + "', '" + coursecode(i) + "', '" + modulename(i) + "', '" + moduledesc(i) + "')", cn)
+                cm.ExecuteNonQuery()
+            Next
+
+            'cm = New OleDbCommand("insert into modulo values ('" + modid.Text + "', '" + crc.Text + "', '" + modname.Text + "', '" + moddesc.Text + "')", cn)
+            'cm.ExecuteNonQuery()
+            cn.Close()
             MsgBox("Got all " & pages & " inputs")
             Me.Close()
         Else
-            cn = c.getcon()
-            cm = New OleDbCommand("insert into modulo values ('" + modid.Text + "', '" + crc.Text + "', '" + modname.Text + "', '" + moddesc.Text + "')", cn)
-            cm.ExecuteNonQuery()
-            cn.Close()
-            clearing()
+                'cn = c.getcon()
+                'cm = New OleDbCommand("insert into modulo values ('" + modid.Text + "', '" + crc.Text + "', '" + modname.Text + "', '" + moddesc.Text + "')", cn)
+                'cm.ExecuteNonQuery()
+                'cn.Close()
+                moduleid(pages - 1) = modid.Text
+                coursecode(pages - 1) = crc.Text
+                modulename(pages - 1) = modname.Text
+                moduledesc(pages - 1) = moddesc.Text
+
+
+                clearing()
+        End If
+    End Sub
+
+    Private Sub modid_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles modid.TextChanged
+        cn = c.getcon()
+        cm = New OleDbCommand("select * from modulo where moduleid = '" + modid.Text + "'", cn)
+        dr = cm.ExecuteReader
+        If dr.HasRows Then
+            MsgBox("sorry the module code already exits")
         End If
     End Sub
 End Class
