2a0b1dedab16b10b895e5ead078ef4339a1255e1
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-03 18:00:00 +0530

    fixed bug of opening new obj conn

--- student-ims/student-ims/Certificate Form.vb
+++ student-ims/student-ims/Certificate Form.vb
@@ -14,6 +14,7 @@ Public Class Certificate_Form
     Dim ds As DataSet
 
     Private Sub Certificate_Form_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        cn = c.getcon()
         If Not cn.State = ConnectionState.Open Then
             cn.Open()
         End If
