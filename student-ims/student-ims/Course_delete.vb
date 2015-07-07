1988983050e4071f87ebf4d6c9c60535e6d47feb
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-07 17:14:28 +0530

    Add database imports and vars

--- student-ims/student-ims/Course_delete.vb
+++ student-ims/student-ims/Course_delete.vb
@@ -1,3 +1,13 @@
-﻿Public Class Course_delete
+﻿Imports System.Data.OleDb
 
+Public Class Course_delete
+    Dim cn As OleDbConnection
+    Dim cm As OleDbCommand
+    Dim dr As OleDbDataReader
+    Dim c As New conn
+    Dim cnt As Integer
+
+    Private Sub Course_delete_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
