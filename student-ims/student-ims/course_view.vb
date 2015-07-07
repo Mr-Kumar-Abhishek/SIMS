4dd4613d0fafb19baab48e9a740fd34c1613bba6
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-07 16:53:44 +0530

    add selection functionality

--- student-ims/student-ims/course_view.vb
+++ student-ims/student-ims/course_view.vb
@@ -12,7 +12,25 @@ Public Class course_view
     End Sub
 
     Private Sub crc_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles crc.TextChanged
-
+        If Not cn.State = ConnectionState.Open Then
+            cn = c.getcon()
+        End If
+        cm = New OleDbCommand("select coursename from course where coursecode = '" + crc.Text + "'", cn)
+        dr = cm.ExecuteReader
+        While (dr.Read)
+            crn.Text = dr.GetValue(0)
+        End While
+        cm = New OleDbCommand("select totalfees from course where coursecode = '" + crc.Text + "'", cn)
+        dr = cm.ExecuteReader
+        While (dr.Read)
+            tf.Text = dr.GetValue(0)
+        End While
+        cm = New OleDbCommand("select duration from course where coursecode = '" + crc.Text + "'", cn)
+        dr = cm.ExecuteReader
+        While (dr.Read)
+            dur.Value = dr.GetValue(0)
+        End While
+        cn.Close()
     End Sub
 
     Private Sub course_view_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
