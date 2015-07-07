11340202fa4f6415c7a82d1739cc40a181d9954e
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-07 16:44:36 +0530

    add connection vars

--- student-ims/student-ims/course_view.vb
+++ student-ims/student-ims/course_view.vb
@@ -1,5 +1,10 @@
 ﻿Imports System.Data.OleDb
 
 Public Class course_view
+    Dim cn As OleDbConnection
+    Dim cm As OleDbCommand
+    Dim dr As OleDbDataReader
+    Dim c As New conn
+    Dim cnt As Integer
 
 End Class
