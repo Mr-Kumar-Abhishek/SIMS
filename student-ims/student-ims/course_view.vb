60d2e34898ad449fc4b86f36ce5e4488248e1392
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-07 16:55:26 +0530

    Add course code list population

--- student-ims/student-ims/course_view.vb
+++ student-ims/student-ims/course_view.vb
@@ -35,5 +35,11 @@ Public Class course_view
 
     Private Sub course_view_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         cn = c.getcon()
+        cm = New OleDbCommand("select coursecode from course", cn)
+        dr = cm.ExecuteReader
+        While (dr.Read)
+            crc.Items.Add(dr.GetValue(0))
+        End While
+        cn.Close()
     End Sub
 End Class
