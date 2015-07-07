ece49c70e55cbabc1f530a6a0d6235e6fb054091
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-07 16:51:08 +0530

    add connection object

--- student-ims/student-ims/course_view.vb
+++ student-ims/student-ims/course_view.vb
@@ -10,4 +10,12 @@ Public Class course_view
     Private Sub cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cancel.Click
         Me.Hide()
     End Sub
+
+    Private Sub crc_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles crc.TextChanged
+
+    End Sub
+
+    Private Sub course_view_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        cn = c.getcon()
+    End Sub
 End Class
