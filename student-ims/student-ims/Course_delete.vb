ce4ec6d645eda55e764b3ea59eec2e0d2b4b37b4
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-07 17:19:01 +0530

    Add Course list population

--- student-ims/student-ims/Course_delete.vb
+++ student-ims/student-ims/Course_delete.vb
@@ -8,6 +8,12 @@ Public Class Course_delete
     Dim cnt As Integer
 
     Private Sub Course_delete_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
+        cn = c.getcon()
+        cm = New OleDbCommand("select coursecode from course", cn)
+        dr = cm.ExecuteReader
+        While (dr.Read)
+            crc.Items.Add(dr.GetValue(0))
+        End While
+        cn.Close()
     End Sub
 End Class
