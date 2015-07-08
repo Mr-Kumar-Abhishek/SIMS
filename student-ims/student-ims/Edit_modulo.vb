897184f8685e95675de66c27e43e25fa887a5d91
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-08 16:58:41 +0530

    name and description extraction

--- student-ims/student-ims/Edit_modulo.vb
+++ student-ims/student-ims/Edit_modulo.vb
@@ -37,4 +37,23 @@ Public Class Edit_modulo
         End While
         cn.Close()
     End Sub
+
+    Private Sub AvailMod_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AvailMod.SelectedIndexChanged
+        If Not cn.State = ConnectionState.Open Then
+            cn = c.getcon()
+        End If
+        cm = New OleDbCommand("select module_name from modulo where moduleid = '" & AvailMod.Text & "'", cn)
+        dr = cm.ExecuteReader
+        While (dr.Read)
+            modname.Text = dr.GetValue(0)
+        End While
+        cm = New OleDbCommand("select module_desc from modulo where moduleid = '" & AvailMod.Text & "'", cn)
+        dr = cm.ExecuteReader
+        While (dr.Read)
+            moddesc.Text = dr.GetValue(0)
+        End While
+        cn.Close()
+    End Sub
 End Class
+
+
