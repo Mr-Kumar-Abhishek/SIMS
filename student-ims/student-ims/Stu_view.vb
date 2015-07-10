7a97770b64e9a505d9dba4ddebadaae1f724c211
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-10 16:11:24 +0530

    Add clearing funtion to student edit form
    This fucntion will be further used while the form is loaded with all the
    data of the student via function repopulate()

--- student-ims/student-ims/Stu_view.vb
+++ student-ims/student-ims/Stu_view.vb
@@ -1,6 +1,26 @@
 ﻿Public Class Stu_view
 
+
+    Private Sub clearing()
+        stcode.Items.Clear()
+        stname.Text = ""
+        gender.Text = ""
+        qual.Text = ""
+        add.Text = ""
+        dob.Text = ""
+        tele.Text = ""
+        crc.Text = ""
+        stdate.Text = ""
+        endate.Text = ""
+        crcd.Text = ""
+        crf.Text = ""
+    End Sub
+
     Private Sub CheckedListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles modbox.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Stu_view_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
