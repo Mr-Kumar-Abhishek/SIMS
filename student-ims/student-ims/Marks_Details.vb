fb0c656fa050f7b3dcee606260591a7c52a6f835
Abhishek Kumar <kumar.abhishek@techie.com>
2015-07-11 15:41:03 +0530

    Cancel functionality in marks details forms

--- student-ims/student-ims/Marks_Details.vb
+++ student-ims/student-ims/Marks_Details.vb
@@ -72,4 +72,7 @@ Public Class Marks_Details
         End If
     End Sub
 
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
 End Class
